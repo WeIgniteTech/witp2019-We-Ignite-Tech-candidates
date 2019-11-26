2e5e627902d4679a761faa5489aff7d8651c364c
snehasirure <siruresneha@gmail.com>
2019-11-26 22:17:41 +0100

    Added basic web page, question with input field and ability to supply text.

--- src/Frontpage/Frontpage.js
+++ src/Frontpage/Frontpage.js
@@ -3,17 +3,26 @@ import './Frontpage.css';
 import DisplayDate from '../DisplayDate/DisplayDate'
 
 const Frontpage = (props) => {
-    return (
-        <div className="Frontpage">
-        <header className="Frontpage-header">
-          <img src={props.logo} className="Frontpage-logo" alt="logo" />
-          <p>
-            Welcome to your application!
+  return (
+    <div className="Frontpage">
+      <header className="Frontpage-header">
+        <img src={props.logo} className="Frontpage-logo" alt="logo" />
+        <p>
+          We Ignite Tech application
           </p>
-          <DisplayDate/>
-        </header>
-      </div>
-    );
+
+        <form>
+          <label>
+            Hva heter du?
+            <br />
+            <input type="text" name="name" />
+          </label>
+          <br />
+          <input type="submit" value="SEND" onClick="#"/>
+        </form>
+      </header>
+    </div>
+  );
 };
 
 export default Frontpage;
