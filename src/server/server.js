04f97787afeb032b3022a410d0dce6fb40e050d1
Carl B <petter@axesnet.com>
2019-11-27 17:15:06 +0100

    post in koa

--- src/server/server.js
+++ src/server/server.js
@@ -11,23 +11,23 @@ const mount = require("koa-mount");
 const app = new Koa();
 const PORT = process.env.PORT || 3000;
 
-/* 
-  This is the static part of the server: 
+/*
+  This is the static part of the server:
    it serves the 'build' directory
 */
 const static_pages = new Koa();
-static_pages.use(serve(__dirname + "/../../build")); 
+static_pages.use(serve(__dirname + "/../../build"));
 app.use(mount("/", static_pages));
 
 app.use(BodyParser());
 app.use(Logger());
 
 
-/* 
+/*
   This is the API / dynamic part of the application
 */
 
-/* 
+/*
   '/healthcheck' endpoint
   Returns "running" if everything is OK
 */
@@ -40,9 +40,9 @@ const healthcheck = router.get('/healthcheck',
 })
 app.use(healthcheck);
 
-/* 
+/*
   '/api/now' endpoint
-  This endpoint returns the actual date
+  This endpoint will add info about our candidate
 */
 const now = router.get('/api/now',
   (ctx) => {
@@ -56,7 +56,24 @@ app.use(now);
 
 
 /*
- This is where we start the server 
+  '/api/candidate' endpoint
+  This endpoint returns the actual date
+*/
+const new_cand = router.post('/api/candidates',
+  (ctx) => {
+      ctx.status = HttpStatus.OK;
+      console.log('path: ', ctx.path);
+      console.log('query: ', ctx.query);
+      console.log('body: ', ctx.request.body);
+      console.log('ctx: ', ctx);
+      // ctx.status = HttpStatus.OK;
+      const date = new Date();
+      ctx.body = ctx.request.body;
+})
+app.use(new_cand);
+
+/*
+ This is where we start the server
 */
 app.listen(PORT, function () {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
