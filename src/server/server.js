62207e02570b308bece26dd73e9d40ddaeacc51e
Thomas Dufourd <thomas.dufourd@gmail.com>
2019-12-07 18:03:02 +0100

    Fix: returning sthg after POST request to Airtable is finished

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ const router = require('koa-route');
 const mount = require("koa-mount");
 const axios = require("axios");
 var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'keyn3fIZfJ9dEX8Px'}).base('appUdJOf889CrTa8y');
+var base = new Airtable({ apiKey: 'keyn3fIZfJ9dEX8Px' }).base('appUdJOf889CrTa8y');
 
 
 const app = new Koa();
@@ -38,11 +38,11 @@ app.use(Logger());
 */
 const healthcheck = router.get('/healthcheck',
   (ctx) => {
-      console.log("Healthcheck handler", ctx.path);
-      console.log(ctx.query);
-      ctx.status = HttpStatus.OK;
-      ctx.body = "Running";
-})
+    console.log("Healthcheck handler", ctx.path);
+    console.log(ctx.query);
+    ctx.status = HttpStatus.OK;
+    ctx.body = "Running";
+  })
 app.use(healthcheck);
 
 /*
@@ -51,21 +51,21 @@ app.use(healthcheck);
 */
 const now = router.get('/api/now',
   (ctx) => {
-      console.log('path: ', ctx.path);
-      console.log('query: ', ctx.query);
-      ctx.status = HttpStatus.OK;
-      const date = new Date();
-      ctx.body = `{"today": "${date}"}`;
-})
+    console.log('path: ', ctx.path);
+    console.log('query: ', ctx.query);
+    ctx.status = HttpStatus.OK;
+    const date = new Date();
+    ctx.body = `{"today": "${date}"}`;
+  })
 app.use(now);
 
 const healthcheck2 = router.get('/pepe',
   (ctx) => {
-      console.log("Healthcheck handler", ctx.path);
-      console.log(ctx.query);
-      ctx.status = HttpStatus.OK;
-      ctx.body = "pepe is home";
-})
+    console.log("Healthcheck handler", ctx.path);
+    console.log(ctx.query);
+    ctx.status = HttpStatus.OK;
+    ctx.body = "pepe is home";
+  })
 app.use(healthcheck2);
 
 
@@ -75,76 +75,47 @@ app.use(healthcheck2);
   This endpoint returns the actual date
 */
 const new_cand = router.post('/api/candidates2',
-   (ctx) => {
-      // try {
-      //     const record = await postCandidate();
-      //     console.log('Retrieved', record.id);
-      //     ctx.body = "Record ID from API: " + record.id;
-      // } catch (err) {
-      //     // handle exception
-      // }
+  (ctx) => {
+    candidateObject = ctx.request.body;
 
-      // ctx.status = HttpStatus.OK;
-      // // console.log('path: ', ctx.path);
-      // // console.log('query: ', ctx.query);
-      // console.log('body: ', ctx.request.body);
-      // // // console.log('ctx: ', ctx);
-      // // // ctx.status = HttpStatus.OK;
-      // // // const date = new Date();
-      // ctx.body = ctx.request.body;
-
-      candidateObject = ctx.request.body;
-
-      const config = {
-       // mode: 'no-cors',
-         headers: {
-           'Content-Type': 'application/json',
-           'Authorization':'Bearer keyn3fIZfJ9dEX8Px'
-         }
-       }
-
-      // const records = postCandidate();
-      // console.log("records:",records);
-      // ctx.status = HttpStatus.OK;
-      // ctx.body = ctx.request.body;
-
-        axios
-      .post('https://api.airtable.com/v0/appUdJOf889CrTa8y/candidates',candidateObject,config)
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer keyn3fIZfJ9dEX8Px'
+      }
+    }
+    axios
+      .post('https://api.airtable.com/v0/appUdJOf889CrTa8y/candidates', candidateObject, config)
       .then(res => {
         ctx.status = HttpStatus.OK;
-        console.log(res.data);
-
-      })
+        //console.log(res.data);
+        console.log('Got some data, POST is finished !');
+        ctx.body = 'OK';
+      }).then(() => {
+        console.log('returning OK');
+        ctx.body = 'OK2';
+      }
+      )
       .catch(function (error) {
-      console.log(error);
+        console.log(error);
       })
     // ctx.preventDefault()
 
-      // You can also read other commands to AirTable here: https://flaviocopes.com/airtable/
+    // You can also read other commands to AirTable here: https://flaviocopes.com/airtable/
 
-})
+  })
 app.use(new_cand);
 
 app.use(router.post('/api/candidates', async function (ctx) {
   try {
     const record = await postCandidate();
-    console.log(`Retrieved ${records.length} records`);
     ctx.status = HttpStatus.OK;
-    ctx.body = ctx.request.body;
+    ctx.body = '{"status": 200, "message": "Content saved in the database"}';
     let candidateList = record
-    // let candidateList = [];
-    // records.forEach((record) => {
-    //   playersList.push({
-    //     "name": record.fields.name,
-    //     "age": false
-    //   });
-    // });
-
-    ctx.body = candidateList;
   } catch (err) {
     console.log('Got an error: ', err);
-    ctx.body = "Failed :( ";
-    // handle exception
+    ctx.status = HttpStatus.INTERNAL_SERVER_ERROR;
+    ctx.body = '{"status": 500, "message": "Could not save the data"}';
   }
 }));
 
@@ -157,15 +128,14 @@ function postCandidate() {
           "age": 21
         }
       }
-    ], function(err, record) {
+    ], function (err, record) {
       if (err) {
         console.error(err);
-        return;
-      }
-      // records.forEach(function (record) {
+        reject(err);
+      } else {
         console.log(record);
-        // return record.getId()
-      // });
+        resolve(record);
+      }
     });
   });
 }
@@ -173,5 +143,5 @@ function postCandidate() {
  This is where we start the server
 */
 app.listen(PORT, function () {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
+  console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
 });
