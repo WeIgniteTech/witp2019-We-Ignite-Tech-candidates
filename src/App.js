6b3b92d538e44e26675989c5a3e0d8702432004d
Carl B <petter@axesnet.com>
2019-12-08 12:53:58 +0100

    now working form forntpage.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import Frontpage from './Frontpage/Frontpage2';
+import Frontpage from './Frontpage/Frontpage';
 import ApiDateResourceProvider from './ApiDateResourceProvider/ApiDateResourceProvider';
 
 function App() {
